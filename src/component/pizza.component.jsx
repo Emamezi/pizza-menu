7cf55ba3f0d1fe54d680d22da58037121ac8f42b
Mezi <emameziebebinwe@gmail.com>
2024-08-06 12:13:00 -0400

    added sold out functionality and changed app title

--- src/component/pizza.component.jsx
+++ src/component/pizza.component.jsx
@@ -1,13 +1,12 @@
 const Pizza = ({ pizza }) => {
-  const { photoName, name, ingredients, price } = pizza;
-
+  const { photoName, name, ingredients, price, soldOut } = pizza;
   return (
-    <li className="pizza">
+    <li className={`pizza ${soldOut ? "sold-out" : ""}`}>
       <img src={photoName} alt="pizza-image" />
       <div>
         <h3>{name}</h3>
         <p>{ingredients}</p>
-        <span>${price}</span>
+        <span>{soldOut ? "SOLD-OUT" : price}</span>
         <span></span>
       </div>
     </li>
